9718a99599879a26d200d07d16a53bf2392b4f5c
Andrey <werwwolf4@yandex.ru>
2024-08-25 18:06:58 +0500

    create

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-example = 'Злоймотыль'
-print(example[0])
-print(example[-1])
-print(example[:4])
-print(example[1::2])
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = sorted(list({'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}))
+average_rating = {}
+for i in range(len(students)):
+    average_rating[students[i]] = sum(grades[i]) / len(grades[i])
+print(average_rating)
